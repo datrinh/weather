330ef4c90c32372453d14e1038b96cee4c72567e
Duc Anh Trinh <ducanh.trinh@hotmail.de>
2019-10-10 09:18:12 +0200

    added time logic in weather service

--- src/app/shared/weather.service.ts
+++ src/app/shared/weather.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { GeocodingService } from './geocoding.service';
 import { Coordinates } from './geocoding.service';
 import { DarkSkyResponse } from './DarkSkyResponse';
-import { flatMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 // Quick Fix for CORS problems
@@ -20,9 +18,20 @@ const API_KEY = environment.darkSkyApiKey;
 export class WeatherService {
   constructor(private http: HttpClient) {}
 
-  getWeatherData(coords: Coordinates): Observable<DarkSkyResponse> {
-    return this.http.get<DarkSkyResponse>(
-      `${CORS_PREFIX}/${API_URL}/${API_KEY}/${coords.latitude},${coords.longitude}?units=ca`
-    );
+  /**
+   * Use DarkSkyApi to retrive weather data for speficic coords
+   * @param coords latitude and longitude for location
+   * @param time Either UNIX or [YYYY]-[MM]-[DD]T[HH]:[MM]:[SS][timezone]
+   */
+  getWeatherData(
+    coords: Coordinates,
+    time?: string
+  ): Observable<DarkSkyResponse> {
+    const request = `${CORS_PREFIX}/${API_URL}/${API_KEY}/${coords.latitude},${coords.longitude}`;
+    if (time) {
+      return this.http.get<DarkSkyResponse>(`${request},${time}?units=ca`);
+    } else {
+      return this.http.get<DarkSkyResponse>(request + '?units=ca');
+    }
   }
 }
